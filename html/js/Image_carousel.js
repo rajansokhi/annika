3c19058ad7b2ab7b80c3578c59f709e8813b2912
Rajan.sokhi <Rajansokhi@hotmail.com>
2014-03-05 00:36:48 +0000

    collections animation and menu alignment for smaller breakpoints

---
+++ html/js/Image_carousel.js
@@ -0,0 +1,18 @@
+        function carousel_bg(id) {
+                var bgimgs = [ 'images/1.jpg', 'images/2.jpg', 'images/3.jpg' ]; // add images here..
+                var img = bgimgs[id];
+                var cnt = 3; // change this number when adding images..
+				
+				$('#home').fadeTo('slow', 0.3, function() {
+				$(this).css("background-image", "url("+img+")");
+				}).fadeTo('slow', 1);
+				
+                id = id + 1;
+                if (id==cnt) id = 0;
+
+                setTimeout("carousel_bg("+id+")", 7000);
+        }
+
+        $(document).ready(function() {
+                carousel_bg(0);     
+        });
